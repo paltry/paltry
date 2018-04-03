c3f02890a3ec003e91b7441ef3564b1eb0f5e407
Wolfgang <wolf@okwolf.com>
2018-04-03 14:56:37 -0700

    fix smartgit download

--- plugins/smartgit.ps1
+++ plugins/smartgit.ps1
@@ -1,8 +1,8 @@
 if($Online) {
   $SmartGitDownloadUrl = Invoke-WebRequest -Uri "https://www.syntevo.com/smartgit/download" |
     %{ $_.Links } | %{ $_.href } |
-    %{ ([regex]'smartgit-portable-[\d_]+.7z$').Matches($_) | %{ $_.Value } } |
-    %{ "https://www.syntevo.com/static/smart/download/smartgit/$_" } | Select-Object -First 1
+    %{ ([regex]'.*smartgit-portable-[\d_]+.7z$').Matches($_) | %{ $_.Value } } |
+    %{ "https://www.syntevo.com$_" } | Select-Object -First 1
 }
 InstallTool -Name "SmartGit" -Url $SmartGitDownloadUrl -Prefix smartgit*
 Add-Launch -Name "SmartGit" -Target "$(FindTool smartgit*)\bin\smartgit.exe"
