4054fac971c60120018eb1d7eb100787e0c4c7e6
Wolfgang <wolf@okwolf.com>
2018-04-06 10:04:22 -0700

    custom maven version support

--- plugins/maven.ps1
+++ plugins/maven.ps1
@@ -1,6 +1,15 @@
+param (
+  [string]$Version,
+  [bool]$UseLatestVersion
+)
+
 if($Online) {
-  $MavenDownloadUrl = Invoke-WebRequest -Uri "https://maven.apache.org/download.cgi" |
-    %{ $_.Links } | %{ $_.href } | ?{ $_ -Match "apache-maven-[\d.]+-bin.zip$" }
+  if($UseLatestVersion) {
+    $MavenDownloadUrl = Invoke-WebRequest -Uri "https://maven.apache.org/download.cgi" |
+      %{ $_.Links } | %{ $_.href } | ?{ $_ -Match "apache-maven-[\d.]+-bin.zip$" }
+  } else {
+    $MavenDownloadUrl = "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=maven/maven-3/$Version/binaries/apache-maven-$Version-bin.zip"
+  }
 }
 InstallTool -Name "Maven" -Url $MavenDownloadUrl -Prefix apache-maven*
 
@@ -8,6 +17,7 @@ $MavenUserFolder = "$UserProfile\.m2"
 $MavenRepo = "$MavenUserFolder\repository"
 
 if(Test-Path $MavenRepo) {
+  Out-Info "Cleaning up remote m2 repo data..."
   Get-ChildItem -Recurse $MavenRepo -Include @("_maven.repositories", "_remote.repositories",
     "maven-metadata-local.xml", "*.lastUpdated", "resolver-status.properties") |
     %{ Remove-Item -Force -ErrorAction Ignore $_ }
