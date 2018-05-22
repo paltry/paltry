0d31a557ebbcd75a60a378f56450636c3456e3ca
Wolfgang <wolf@okwolf.com>
2018-05-22 15:51:26 -0700

    update for new maven download page

--- plugins/maven.ps1
+++ plugins/maven.ps1
@@ -3,22 +3,20 @@ param (
   [bool]$UseLatestVersion
 )
 
-if($Online) {
-  if($UseLatestVersion) {
-    $MavenDownloadUrl = Invoke-WebRequest -Uri "https://maven.apache.org/download.cgi" |
-      %{ $_.Links } | %{ $_.href } | ?{ $_ -Match "apache-maven-[\d.]+-bin.zip$" }
-  } else {
-    $MavenDownloadUrl = "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=maven/maven-3/$Version/binaries/apache-maven-$Version-bin.zip"
-  }
+if($Online -And $UseLatestVersion) {
+  $Version = Invoke-WebRequest -Uri "https://archive.apache.org/dist/maven/maven-3" |
+    %{ $_.Links } | %{ $_.innerText -Replace "/", "" } | ?{ $_ -Match "3." } |
+    Sort-Object -Descending | Select-Object -First 1
 }
+$MavenDownloadUrl = "https://archive.apache.org/dist/maven/maven-3/$Version/binaries/apache-maven-$Version-bin.zip"
 InstallTool -Name "Maven" -Url $MavenDownloadUrl -Prefix apache-maven*
 
 $MavenUserFolder = "$UserProfile\.m2"
 $MavenRepo = "$MavenUserFolder\repository"
 
 if(Test-Path $MavenRepo) {
-  Out-Info "Cleaning up remote m2 repo data..."
-  Get-ChildItem -Recurse $MavenRepo -Include @("_maven.repositories", "_remote.repositories",
-    "maven-metadata-local.xml", "*.lastUpdated", "resolver-status.properties") |
-    %{ Remove-Item -Force -ErrorAction Ignore $_ }
+ Out-Info "Cleaning up remote m2 repo data..."
+ Get-ChildItem -Recurse $MavenRepo -Include @("_maven.repositories", "_remote.repositories",
+   "maven-metadata-local.xml", "*.lastUpdated", "resolver-status.properties") |
+   %{ Remove-Item -Force -ErrorAction Ignore $_ }
 }
