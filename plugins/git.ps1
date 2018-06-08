3634572d9cf51b49699181ca8e853df030df3ca7
Wolfgang <wolf@okwolf.com>
2018-06-07 18:29:09 -0700

    upgrading from mingit to portable git

--- plugins/git.ps1
+++ plugins/git.ps1
@@ -1,7 +1,7 @@
 if($Online) {
   $GitReleaseApiUrl = "https://api.github.com/repos/git-for-windows/git/releases/latest"
-  $MinGitRelease = DownloadString $GitReleaseApiUrl | ConvertFrom-Json |
-    Select-Object -Expand assets | Where-Object { $_.name -Match "MinGit.*64-bit.zip" }
-    $MinGitUrl = $MinGitRelease.browser_download_url -Split " " | Select-Object -First 1
+  $PortableGitRelease = (DownloadString $GitReleaseApiUrl | ConvertFrom-Json) |
+    Select-Object -Expand assets | Where { $_.name -Match "PortableGit.+64-bit.+" }
+  $PortableGitUrl = $PortableGitRelease.browser_download_url -Split " " | Select-Object -First 1
 }
-InstallTool -Name "Git" -Url $MinGitUrl -Prefix MinGit*
+InstallTool -Name "Git" -Url $PortableGitUrl -Prefix PortableGit*
