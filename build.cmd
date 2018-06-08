ccb985a8888649c7217f7616bf367115a310ab61
Wolfgang <wolf@okwolf.com>
2018-06-07 19:57:07 -0700

    allow executing the scripts from downloads for bootstrapping

--- build.cmd
+++ build.cmd
@@ -5,7 +5,7 @@ set TMP_SCRIPT="%TMP%\%~n0.ps1"
 for /f "delims=:" %%a in ('findstr -n "^___" %0') do set "Line=%%a"
 (for /f "skip=%Line% tokens=* eol=_" %%a in ('type %0') do echo(%%a) > %TMP_SCRIPT%
 
-powershell -ExecutionPolicy RemoteSigned -File %TMP_SCRIPT%
+powershell -ExecutionPolicy Bypass -File %TMP_SCRIPT%
 exit
 
 ___SCRIPT___
