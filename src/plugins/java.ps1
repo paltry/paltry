b92831442d45251dbae055bf1b692e881069b72c
Wolfgang <wolf@okwolf.com>
2020-05-12 19:51:06 -0700

    switching openjdk repos

--- src/plugins/java.ps1
+++ src/plugins/java.ps1
@@ -1,7 +1,7 @@
 if ($Online) {
-  $JavaReleaseApiUrl = "https://api.github.com/repos/AdoptOpenJDK/openjdk8-binaries/releases/latest"
+  $JavaReleaseApiUrl = "https://api.github.com/repos/ojdkbuild/ojdkbuild/releases/latest"
   $JavaRelease = (DownloadString $JavaReleaseApiUrl | ConvertFrom-Json) |
-  Select-Object -Expand assets | Where-Object { $_.Name -match "jdk_x64_windows_.+\.zip" }
+  Select-Object -Expand assets | Where-Object { $_.Name -match "openjdk-1.8.0.+\.windows\.x86_64\.zip" }
   $JavaDownloadUrl = $JavaRelease.browser_download_url -split " " | Select-Object -First 1
 }
 
