a1be629102e4d0f2b4ac49e28aca0b4a120f9999
Wolfgang <wolf@okwolf.com>
2020-08-25 22:39:20 -0700

    updating wiztree for new download page

--- src/plugins/wiztree.ps1
+++ src/plugins/wiztree.ps1
@@ -1,8 +1,10 @@
 if ($Online) {
-  $WizTreeDownloadUrl = Get-WebRequest "https://antibody-software.com/web/software/software/wiztree-finds-the-files-and-folders-using-the-most-disk-space-on-your-hard-drive" |
-  ForEach-Object { $_.Links } | ForEach-Object { $_.href } |
+  $WizTreeDownloadPrefix = "https://wiztreefree.com"
+  $WizTreeDownloadPath = Get-WebRequest "$WizTreeDownloadPrefix/download" |
+  ForEach-Object { $_.Links } | ForEach-Object { $_.href -replace "about:","" } |
   ForEach-Object { ([regex]'.*wiztree_[\d_]+_portable.zip$').Matches($_) |
     ForEach-Object { $_.Value } } | Select-Object -First 1
+  $WizTreeDownloadUrl = "$WizTreeDownloadPrefix/$WizTreeDownloadPath"
 }
 InstallTool -Name "WizTree" -Url $WizTreeDownloadUrl -Prefix wiztree*
 Add-Launch -Name "WizTree" -Target "$(FindTool wiztree*)\WizTree64.exe"
