ee1b30f76419455da791fa0399d98c11bd1eb58c
Wolfgang <wolf@okwolf.com>
2020-10-29 21:25:08 -0700

    java release naming fix

--- src/plugins/java.ps1
+++ src/plugins/java.ps1
@@ -1,7 +1,7 @@
 if ($Online) {
   $JavaReleaseApiUrl = "https://api.github.com/repos/ojdkbuild/ojdkbuild/releases/latest"
   $JavaRelease = (DownloadString $JavaReleaseApiUrl | ConvertFrom-Json) |
-  Select-Object -Expand assets | Where-Object { $_.Name -match "openjdk-1\.8\.0.+\.windows\.x86\.zip" }
+  Select-Object -Expand assets | Where-Object { $_.Name -match "java-1\.8\.0.+\.windows\.x86_64\.zip" }
   $JavaDownloadUrl = $JavaRelease.browser_download_url -split " " | Select-Object -First 1
 }
 
