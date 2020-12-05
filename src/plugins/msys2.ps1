7601bfe1b8fe753e193542bfb0c7282e7f650bd1
Wolfgang <wolf@okwolf.com>
2020-12-05 13:51:08 -0800

    fix for msys2 download

--- src/plugins/msys2.ps1
+++ src/plugins/msys2.ps1
@@ -1,6 +1,6 @@
 if ($Online) {
   $MSYS2RepoUrl = "http://repo.msys2.org/distrib/x86_64"
-  $MSYS2File = Get-WebRequest $MSYS2RepoUrl | ForEach-Object { $_.Links } | ForEach-Object { $_.href -replace "about:","" } | Where-Object { $_ -match "msys2-base-x86_64-.+\.tar\.xz" } |
+  $MSYS2File = Get-WebRequest $MSYS2RepoUrl | ForEach-Object { $_.Links } | ForEach-Object { $_.href -replace "about:","" } | Where-Object { $_ -match "msys2-base-x86_64-.+\.tar\.xz$" } |
   Sort-Object -Descending | Select-Object -First 1
   $MSYS2DownloadUrl = "$MSYS2RepoUrl/$MSYS2File"
 }
