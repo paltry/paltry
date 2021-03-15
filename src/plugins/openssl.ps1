666f6e7579a6a796bec783b848d150730b4c9d37
Wolfgang <wolf@okwolf.com>
2021-03-15 00:55:11 -0700

    update openssl

--- src/plugins/openssl.ps1
+++ src/plugins/openssl.ps1
@@ -1,26 +1,11 @@
-$OpenSslDownloadSite = "https://bintray.com/vszakats/generic"
+$OpenSslDownloadSite = "https://indy.fulgan.com/SSL"
 if ($Online) {
-  $RedirectRequest = [System.Net.WebRequest]::Create("$OpenSslDownloadSite/openssl/_latestVersion#files")
-  $RedirectRequest.AllowAutoRedirect = $false
-  $RedirectResponse = $RedirectRequest.GetResponse()
-  $RedirectUrl = $RedirectResponse.GetResponseHeader("Location")
-  $OpenSslVersion = $RedirectUrl.Split("/") | Select-Object -Last 1
-  $OpenSslFile = "openssl-$OpenSslVersion-win64-mingw.zip"
-  $OpenSslFolder = [io.path]::GetFileNameWithoutExtension($OpenSslFile)
-  $OpenSslUrl = "$OpenSslDownloadSite/download_file?file_path=$OpenSslFile"
-  $OpenSslDownloadFile = "$DownloadsFolder\$OpenSslFile"
-  if (!(Test-Path "$ToolsFolder\$OpenSslFolder")) {
-    $WebClient = Get-WebClient
-    if (!(Test-Path $OpenSslDownloadFile)) {
-      Confirm-Online
-      Out-Info "Downloading OpenSSL..."
-      $WebClient.DownloadFile($OpenSslUrl,$OpenSslDownloadFile)
-    }
-    Out-Info "Extracting OpenSSL..."
-    7z x "$OpenSslDownloadFile" -o"$ToolsFolder"
-  }
+  $OpenSslFile = Get-WebRequest $OpenSslDownloadSite |
+  ForEach-Object { $_.Links } | ForEach-Object { $_.href } |
+  ForEach-Object { $_ -replace "about:","" } |
+  Where-Object { $_ -match "openssl-.+-x64_86-win64\.zip$" } |
+  Sort-Object -Descending | Select-Object -First 1
+  $OpenSslDownloadUrl = "$OpenSslDownloadSite/$OpenSslFile"
 }
 
-$OpenSslInstalledFolder = FindTool openssl*
-FindBinAndAddToPath $OpenSslInstalledFolder
-AddEnvExtension "OPENSSL_CONF" "$OpenSslInstalledFolder\openssl.cnf"
+InstallTool -Name "OpenSSL" -Url $OpenSslDownloadUrl -Prefix openssl* | Out-Null
