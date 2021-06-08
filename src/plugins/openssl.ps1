5eec5fdb4b6b2910fbcd9082391256a18bd9c28b
Wolfgang <wolf@okwolf.com>
2021-06-08 13:51:49 -0700

    openssl updates

--- src/plugins/openssl.ps1
+++ src/plugins/openssl.ps1
@@ -1,11 +1,10 @@
-$OpenSslDownloadSite = "https://indy.fulgan.com/SSL"
+$OpenSslDownloadSite = "https://indy.fulgan.com/SSL/Archive/Experimental"
 if ($Online) {
   $OpenSslFile = Get-WebRequest $OpenSslDownloadSite |
   ForEach-Object { $_.Links } | ForEach-Object { $_.href } |
   ForEach-Object { $_ -replace "about:","" } |
-  Where-Object { $_ -match "openssl-.+-x64_86-win64\.zip$" } |
+  Where-Object { $_ -match "openssl-.+-x64-VC2017\.zip$" } |
   Sort-Object -Descending | Select-Object -First 1
   $OpenSslDownloadUrl = "$OpenSslDownloadSite/$OpenSslFile"
 }
-
 InstallTool -Name "OpenSSL" -Url $OpenSslDownloadUrl -Prefix openssl* | Out-Null
